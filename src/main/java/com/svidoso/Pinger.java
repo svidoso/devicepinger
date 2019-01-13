90a61a34ac5a54d05561f16d1fb252ddfe72b1fb
Timo Schulze <schulzetimo@gmail.com>
2019-01-13 16:55:36 +0100

    change to ping process

--- src/main/java/com/svidoso/Pinger.java
+++ src/main/java/com/svidoso/Pinger.java
@@ -3,10 +3,11 @@ package com.svidoso;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.InetAddress;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class Pinger {
 
@@ -34,15 +35,17 @@ public class Pinger {
         worker.interrupt();
     }
 
-    private void loop(){
-        while(!quit){
+    private void loop() {
+        while (!quit) {
             hostsPerVar.forEach((key, value) -> {
                 try {
-                    if (value.stream().anyMatch(this::isReachable)) {
-                        webUpdater.updateVariable(key, System.currentTimeMillis()/1000L);
+                    Optional<String> first = value.stream().filter(this::isReachable).findFirst();
+                    if (first.isPresent()) {
+                        log.info("{} online.", first.get());
+                        webUpdater.updateVariable(key, System.currentTimeMillis() / 1000L);
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    log.error(e.getMessage());
                 }
             });
             try {
@@ -53,14 +56,28 @@ public class Pinger {
         }
     }
 
-    private boolean isReachable(String host){
+//    private boolean isReachable(String host){
+//        try {
+//            InetAddress address = InetAddress.getByName(host);
+//            return address.isReachable(500);
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//        }
+//        return false;
+//    }
+
+    private boolean isReachable(String host) {
+        // in case of Linux change the 'n' to 'c'
+        Process p1 = null;
+        int returnVal = 1;
         try {
-            InetAddress address = InetAddress.getByName(host);
-            return address.isReachable(500);
-        } catch (Exception e) {
+            // change to -n for windows
+            p1 = Runtime.getRuntime().exec("ping -c 1 " + host);
+            returnVal = p1.waitFor();
+        } catch (IOException | InterruptedException e) {
             log.error(e.getMessage());
         }
-        return false;
+        return (returnVal == 0);
     }
 
 }
